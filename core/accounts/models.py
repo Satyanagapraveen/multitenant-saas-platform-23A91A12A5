dc26f6fbc57a5433b2af31105b3b4d802444f706
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2025-12-26 23:25:53 +0530

    feat(accounts): Add custom User model with tenant relationship

---
+++ core/accounts/models.py
@@ -0,0 +1,58 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('role', 'super_admin')
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        return self.create_user(email, password, **extra_fields)
+    
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('super_admin', 'Super Admin'),
+        ('tenant_admin', 'Tenant Admin'),
+        ('user', 'User'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tenant = models.ForeignKey(
+        'tenants.Tenant',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    email = models.EmailField()
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager() 
+
+    # REQUIRED BY DJANGO
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        unique_together = ('tenant', 'email')
+
+    def __str__(self):
+        return self.email
+
