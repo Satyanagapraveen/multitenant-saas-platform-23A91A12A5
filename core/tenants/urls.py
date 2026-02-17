fd76612111e93be1ba07e1aed2b9743ec81db043
SATYA NAGA PRAVEEN KUMAR NAMBURI <153361064+Satyanagapraveen@users.noreply.github.com>
2026-02-17 09:11:05 +0530

    Fix tenant detail URL path

--- core/tenants/urls.py
+++ core/tenants/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_tenants, name='list_tenants'),
-    path('<uuid:tenant_id>', views.tenant_detail, name='tenant_detail'),
+    path('/<uuid:tenant_id>', views.tenant_detail, name='tenant_detail'),
 ]
